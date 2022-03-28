d6ffe4c607d9060eac57b6ac698102a92b7aef85
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 19:57:16 -0300

    Math functions a few handy math functions in python

---
+++ MathFunctions.py
@@ -0,0 +1,14 @@
+import math
+
+pi = 3.14
+
+x, y, z = 1, 2, 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+print(math.sqrt(pi))
+print(max(x, y, z))
+print(min(x, y, z))
