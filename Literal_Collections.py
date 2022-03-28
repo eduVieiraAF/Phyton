5604ea7f54377bd918c48433a003bcb29b35849c
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 19:21:25 -0300

    Literal Collections

---
+++ Literal_Collections.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "mango", "orange", "Pitaya"] #list
+numbers = (1, 2, 3, 4) #tuple
+alphabets = {'a':'apple', 'b':'ball', 'c':'cats'} #dictionary
+vowels = {'a', 'e', 'i', 'o', 'u'}
+
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
