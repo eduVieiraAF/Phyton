9bdd9d3859b73227661ba30114e701a8f84be4b9
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 10:32:12 -0300

    Multiple assignments
    how to assign multiple variables at the same time in one line of code.

---
+++ MultipleAssignments.py
@@ -0,0 +1,27 @@
+# Multiple Assignments allows us to assign multiple variables in one line of code
+
+# regular variable declaration
+# name = "Eduardo"
+# age = 40
+# handsome = True
+
+#Mulpuple assignment
+
+name, age, handsome = "Eduardo", 21, True
+
+print(name)
+print(age)
+print(handsome)
+
+print(" ")
+print("------------------")
+print(" ")
+
+# Variables with the same value
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
