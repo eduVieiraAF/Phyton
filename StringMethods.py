2e2d65b3ade25d124f91dcd89058fdd3e2788b79
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 10:49:31 -0300

    String Methods
    Just a few useful string methods available in Python

---
+++ StringMethods.py
@@ -0,0 +1,11 @@
+name = "eduardo VIEITA"
+print(len(name)) # length
+print(name.find("u")) # finds index of
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit()) # returns True or False
+print(name.isalpha()) # in this case it will return "False" bcs I added a space
+print(name.count("d")) # counts how many characters
+print(name.replace("T", "R"))
+print(name*5) # displays variable 5 times
