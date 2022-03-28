80a6529b5c9c885a6fbc810d7046f681fee20d07
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 10:20:16 -0300

    End of basics of Variables

--- Variables.py
+++ Variables.py
@@ -8,10 +8,25 @@ print("Hello, my name's " +  full_name)
 print('---------------------')
 
 age  = 40
-# age = age + 1 or the easier version just below
+# age = age + 1 -> or the easier version just below
 age += 1
 
 print(type(age))
 print(age)
 #type-casting age cuz you can't concatenate int with str
-print ("I'm " + str(age) +" years old") 
+print ("I'm " + str(age) +" years old.") 
+
+print('---------------------')
+
+height = 170.3
+
+print(height)
+print(type(height))
+print("I'm " + str(height) + "cm tall.")
+
+print('---------------------')
+
+human = True
+
+print(type(human))
+print("Am I a human? Absolutely " + str(human))
