8f1388d4856475356b2a8df3296c1bb177eb4281
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 11:12:17 -0300

    Type casting
    How they do it in Python

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+x = 1
+y = 2.22
+z = "3"
+
+x = float(x)
+y = int(y)
+z = int(z)
+
+print(x)
+print(y)
+print(z*5)
+
+print(" ")
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x + ", " + y + " and " + z)
