9779eacf3e4ae37a6f933dcf509ca1668ce09bf4
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-28 11:26:52 -0300

    User input
    Accepting user input in Python

---
+++ UserInput.py
@@ -0,0 +1,13 @@
+from turtle import st
+
+
+name = input('What is your name? ')
+age = int(input("How old are you? "))
+age = age + 1
+height = float(input("How tall are you? "))
+
+print()
+
+print("Hello " + name)
+print("you're " + str(age) + " years old")
+print("you are " + str(height) + "cm tall")
