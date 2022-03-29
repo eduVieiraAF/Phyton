98dd84a95c68857a70767529ee45d947e7a9c5e7
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-29 10:38:53 -0300

    Loop Control Statements

---
+++ ControlStatements.py
@@ -0,0 +1,30 @@
+# LOOP CONTROL STATEMENTS changes the execution sequence of a loop
+
+    # break = terminates loop entirely
+    # continue =  skips to next iterarion of the loop
+    # pass = does nothing, acts as a placeholder
+
+while True:
+
+    name = input("Type in your name: ")
+      
+    if name != "":
+        break
+
+phone_number = "19-98534-7196"
+
+for i in phone_number: 
+
+    if i == "-":
+        continue
+
+    print(i, end="")
+
+print()
+
+for i in range(1,21):
+    
+    if i == 13:
+        pass
+    else:
+        print(str(i) + " ", end="")
