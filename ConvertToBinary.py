548286628057234d0cbcd2b4d765fd8b22b8c3c5
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-30 21:18:39 -0300

    Convert Decimal to Binary
    Simple logic to convert a decinal number into binary code

---
+++ ConvertToBinary.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+
+def convertToBinary(n):
+
+   if n > 1:
+
+       convertToBinary(n//2)
+   
+   print(n % 2,end = '')
+
+# decimal number
+dec = Decimal(input("Enter a number: "))
+
+convertToBinary(dec)
+print()
