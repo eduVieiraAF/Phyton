303f92862efecf33c24ce312a75528945e7a8874
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-31 07:26:12 -0300

    2D lists
    Lists of lists

---
+++ 2D_Lists.py
@@ -0,0 +1,11 @@
+#2D list is a list of lists aka multi-dimensional list
+
+drinks = ["beer", "whiskey", "wine", "gin", "rum", "coffee", "soda"]
+dinner = ["lasgna", "chicken stew", "steak", "pizza", "burger"]
+dessert = ["ice-cream", "pudding", "fudge cake"]
+
+food = [drinks, dinner, dessert]
+
+print(food) # all lists
+print(food[2]) # specific list
+print(food[0][3]) # [list] [item on list]
