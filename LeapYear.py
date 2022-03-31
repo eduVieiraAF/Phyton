2cac01856e05f4d21ffab6b9a3d627c6c8f62063
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-30 21:07:20 -0300

    Leap Year Simple way to check whether year is a leap year or not

---
+++ LeapYear.py
@@ -0,0 +1,13 @@
+year = int(input("Type in a year: "))
+
+if (year % 400 == 0) and (year % 200 == 0):
+    
+    print("{0} is a leap year".format(year))
+
+elif (year % 400 ==0) and (year % 100 != 0):
+    
+    print("{0} is a leap year".format(year))
+
+else:
+
+    print("{0} is not a leap year".format(year))
