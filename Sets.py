014aee62c62bf3f650c361d3cb73441102614b33
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-31 08:46:50 -0300

    Sets
    A little bit on sets in Python

---
+++ Sets.py
@@ -0,0 +1,30 @@
+# a set is a collection that is unsorted, unindexed and does not allow duplicate values
+
+from re import X
+
+
+utensils = {"fork", "spoon", "knife", "mug"}
+dishes = {"bowl", "plate", "mug"}
+dinner_table = utensils.union(dishes) # joins both sets
+
+print(utensils)
+print()
+
+utensils.add("ladle")
+utensils.remove("knife")
+print(utensils)
+print()
+
+utensils.update(dishes) # adds all the items to a new set
+print(utensils)
+print()
+
+##utensils.clear()isso
+##print(utensils)
+##print()
+
+print (dinner_table)
+print()
+
+print(utensils.difference(dishes)) # display what one has and other doesn't
+print(utensils.intersection(dishes)) # displays what they have in common
