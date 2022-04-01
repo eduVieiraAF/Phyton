52c6e6d98ef80a9587c15ba51f9b776ec14c1421
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-01 14:18:13 -0300

    Keywords arguments

---
+++ KeywordArguments.py
@@ -0,0 +1,11 @@
+# Keyword arguments = arguments preceded by an identifier when we pass them
+#                    to a function. The order of the arguments doesn'y matter
+#                    unlike positional arguments
+
+def hello(first, middle, last):
+    print("Hello " + first + " " + middle + " " + last)
+
+hello("Edu", "Phoenix", "Vieira") #using position arguments so order does matter
+hello(last = "Vieira", first = "Eduardo", middle = "Phoenix") # using the keywords, the order
+                                                              # the order doesn't matter
+
