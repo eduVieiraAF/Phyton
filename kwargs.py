85767963656c4e6fa79d90e14ca15e6fa19d64ac
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-01 15:03:03 -0300

    kwargs
    packing it together

---
+++ kwargs.py
@@ -0,0 +1,9 @@
+# a kwarg (short for keyword argumets) is a parameter that will pack all arguments into a dictionary
+
+def hello(**kwargs): #can name it differently, just use **
+    print("Hello " + kwargs['first'] + " " + kwargs['last'])
+    for key, value in kwargs.items():
+        print(value, end=" ")
+
+hello(title = "Mr.", first="Edu", middle="Phoenix", last="Vieira", 
+age = "40 years old", job = "Dev")
