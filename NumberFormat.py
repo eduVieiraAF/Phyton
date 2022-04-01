01b9f36f5ac5d4b572f705bf0be5af1d34d09be3
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-01 16:21:35 -0300

    Number format a few examples

---
+++ NumberFormat.py
@@ -0,0 +1,9 @@
+num1 = 3.14159
+num2 = 1000
+
+print("Pi is {:.2f}".format(num1)) # displays 2 digits
+print("The number is {:,}".format(num2)) # adds a ,
+print("The binary number is {:b}".format(num2)) # converts into binary
+print("The octo number is {:o}".format(num2)) # converto into octo
+print("The hex number is {:X}".format(num2)) # for lowercase use x
+print("The number is {:E}".format(num2)) # displays in scientific notation - lowercase e, as well
