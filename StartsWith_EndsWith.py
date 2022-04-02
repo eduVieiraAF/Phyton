86252b15970c5bc66cfb0adce942a109294207b9
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-02 17:21:07 -0300

    startsWith() & endsWith()
    Simple example

---
+++ StartsWith_EndsWith.py
@@ -0,0 +1,8 @@
+
+# * starstWith() checks if a string starts with certain characters
+# * endsWith() checks if as string ends with certain characters
+
+message = "Code, drink coffe, eat, sleep, repeat"
+
+print(message.startswith('Code')) # output: True
+print(message.endswith('repeat')) # output: True
