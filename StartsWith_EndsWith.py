3223740db427b978161f19ebead0e8c017c0b5fb
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-02 17:23:11 -0300

    Fixed a typo

--- StartsWith_EndsWith.py
+++ StartsWith_EndsWith.py
@@ -2,7 +2,7 @@
 # * starstWith() checks if a string starts with certain characters
 # * endsWith() checks if as string ends with certain characters
 
-message = "Code, drink coffe, eat, sleep, repeat"
+message = "Code, drink coffee, eat, sleep, repeat"
 
 print(message.startswith('Code')) # output: True
 print(message.endswith('repeat')) # output: True
