a823abe82c5eeec4f4ef3fe5031f6441a1855576
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-04 06:16:53 -0300

    Writing in a file

---
+++ WritingFile.py
@@ -0,0 +1,11 @@
+from cgitb import text
+
+text = "A simple way to write files\n using Python.\n"
+
+with open('test2.txt','w') as file:
+    file.write(text)
+    
+text = "New text with append."
+
+with open('test2.txt','a') as file:
+    file.write(text)
