eb2ede9dfe99e7d9afa9ae7cf261c7652159b454
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-04 06:25:36 -0300

    Copying file

---
+++ CopyingFiles.py
@@ -0,0 +1,10 @@
+
+# * copyfile() = copies the content of a file
+# * copy() = copyfile() + permission mode + directory or destination
+# * copy2() = copy + copies metadata (file's creation and modificaition times)
+
+import shutil
+
+shutil.copyfile('test2.txt', 'text2_copy.txt') # source, destination/create file's name
+
+# *other methods use the same 2 arguments as well. 
