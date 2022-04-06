81339a3ccc2da27390fd707a0000eee835ae1e8b
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-06 19:00:48 -0300

    Guessing game
    Consolidating a few notions and lessons

---
+++ QuizzGame.py
@@ -0,0 +1,29 @@
+def new_game():
+    
+    pass
+
+def check_answer():
+    
+    pass
+
+def display_score():
+    
+    pass
+
+def play_again():
+    
+    pass
+
+#* Dictionary
+questions = {
+    "Who created Python?" : "A",
+    "What year was Python created?" : "B",
+    "Which comdey group is Python attributed to?" : "C",
+    "Is the Earth round?" :  "A"}
+
+#* 2-D list
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerberg"],
+        ["A. 1989", "B. 1991", "C. 2001", "D. 2016"],
+        ["A. Lonely Island", "B. Smosh", "C. Monty Python", "Dr. SNL"],
+        "A. True", "B. False", "C. Sometimes", "It is actually hollow"]
+
