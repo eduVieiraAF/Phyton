b92da54fa8be503cca072a42aaee8d0b157edf35
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-13 19:19:45 -0300

    Formatting
    Printing objects using .format()

--- Poop/Main.py
+++ Poop/Main.py
@@ -3,10 +3,7 @@ from Car import Car
 car_1 = Car("Chevy", "Corvette", 2021, "blue")
 car_2 = Car("Honda", "Civic", 2020, "white")
 
-print(car_1.make)
-print(car_1.year)
-print(car_1.model)
-print(car_1.color)
+print("This is a {} {} {} {}. Awesome ride.".format(car_1.color, car_1.year, car_1.make, car_1.model))
 
 print()
 
@@ -15,10 +12,7 @@ car_1.stop()
 
 print()
 
-print(car_2.make)
-print(car_2.year)
-print(car_2.model)
-print(car_2.color)
+print("This is a {} {} {} {}. Awesome ride.".format(car_2.color, car_2.year, car_2.make, car_2.model))
 
 print()
 
