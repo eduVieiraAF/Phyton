8a9bcd19ed96dd7574a81eb0f3ce0ae86461a0d5
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-16 12:25:05 -0300

    Minor changes

--- Samples/KmToMiles.py
+++ Samples/KmToMiles.py
@@ -1,6 +1,5 @@
 kilometers = float(input("Kilometers: "))
 
-conv_factor = 0.621371
-miles = kilometers * conv_factor
+miles = kilometers * 0.621371
 
-print("%0.2f kilometers is equal to %0.2f mile" %(kilometers, miles))
+print("%.2f kilometers is equal to %.2f mile" %(kilometers, miles))
