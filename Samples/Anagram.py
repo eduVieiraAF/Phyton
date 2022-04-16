3646f96544d68d86d4b54dc9796445a42539e127
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-16 14:16:33 -0300

    Improving code

--- Samples/Anagram.py
+++ Samples/Anagram.py
@@ -1,30 +1,21 @@
 print()
 
-word1 = "Race"
-word2 = "Care"
-
-word1 = word1.lower()
-word2 = word2.lower()
+word1 = input("Enter a word: ").lower()
+word2 = input("Enter another word: ").lower()
 
 # Check if strings' length are the same
 if (len(word1) == len(word2)):
-    
     # sort strings
     sorted_word1 = sorted(word1)
     sorted_word2 = sorted(word2)
     
     #check if char sequence is the same
     if (sorted_word1 == sorted_word2):
-        
         print(word1.capitalize() + " and " + word2 + " are an anagram.")
-    
     else:
-        
         print(word1.capitalize() + " and " + word2 + " are NOT an anagram.")
         
 # if strings's lenght are not the same, they're not an anagram
 else:
-    
     print(word1.capitalize() + " and " + word2 + " are NOT an anagram.")
     
-print()
