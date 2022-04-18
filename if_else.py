779833ab56e2b02b506ab74747d11e799366d164
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-18 20:15:28 -0300

    Revisiting and polishing up

--- if_else.py
+++ if_else.py
@@ -2,11 +2,15 @@ age = int(input("How old are you: "))
 
 if age >= 18: 
     print("You're a grown-up")
+    
     if age > 50:
         print("A geezer")
+    
         if age >= 100:
             print("A century is behind you already")
+
 elif age < 0:
     print("A fetus is typing")
+
 else:
     print("You're a minor")    
