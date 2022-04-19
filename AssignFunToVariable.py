76c624d61cdee87b0b2fa2cb2dc972b6e01c8726
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-19 07:44:36 -0300

    Assigning a function to a variablee

---
+++ AssignFunToVariable.py
@@ -0,0 +1,15 @@
+print()
+
+def hello():
+    print("Hello")
+
+hello()
+print(hello) # outputs memory location
+hi = hello #with no brackets, I'm storing the memory address of this function
+print(hi)
+hi() # I'm using bracket to call the function stored in this variable
+
+print()
+
+say = print #assinging the built-in print function to a variable
+say("Magic!")
