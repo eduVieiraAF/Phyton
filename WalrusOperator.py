104f6245fe67c46fece45c3a23cd6e43934a491e
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-19 07:33:19 -0300

    Walrus How to use the walrus operator

---
+++ WalrusOperator.py
@@ -0,0 +1,19 @@
+
+#? Walrus operator := is an assignment expression
+#* assigns values to a variable as part of a larger expression
+
+#happy = True
+#print(happy)
+
+print(happy := True)
+
+foods = list()
+
+#while True:
+#    food = input("What food do you like? ").lower()
+#    if food == "quit":
+#        break
+#    foods.append(food)    without the walrus operator
+
+while food := input("What food do you like? ") != "quit":
+    foods.append(food)
