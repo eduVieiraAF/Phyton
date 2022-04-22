b41e14ce24278a93417e553b102cc629e5d0f62e
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-22 12:36:21 -0300

    for → else

---
+++ ForElse.py
@@ -0,0 +1,14 @@
+people = [{'name': 'John', 'age': 25},
+        {'name': 'Jane', 'age': 22},
+        {'name': 'Peter', 'age': 30},
+        {'name': 'Jenifer', 'age': 28}]
+
+name  = input('Enter a name: ')
+
+for person in people:
+    if person['name'] == name:
+        print("{} is in the list".format(name))
+        break
+    
+else:
+    print("{} is not in the list".format(name))
