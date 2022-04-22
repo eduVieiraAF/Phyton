2b974be83fa58589bd9fad681f7fa05c175db192
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-22 09:20:57 -0300

    Playing with loops

--- z.py
+++ z.py
@@ -1,32 +1,27 @@
-print("----------------------------------------------------------")
-print("««« now playing -> THE BATMAN || Ticket price: A$17.50 »»»")
-print("----------------------------------------------------------")
+counter = 0
+max = 5
 
-tickets = int(input("How many tickets would you like?  "))
-print()
-price = 17.55
+while counter < max:
+    print(counter)
+    sleep(0.5)
+    counter += 1
 
-if tickets > 0 and tickets < 10:
+
+print('--  type QUIT to exit --')
+
+while True:
+    color = input('Enter your favorite color: ')
+    if color.lower() == 'quit':
+        break
+
+name = ""
+
+while name == "":
+    name = input("What's your name? ")
     
-    if tickets == 1:
-        print("Your ticket costs AU${}.".format(price))
-
-    elif tickets == 2:
-        discount = price - (price * 0.03)
-        print("Your tickets cost AU$%.2f a pop, and AU$%.2f total." %(discount, discount*2))
+    if name == "":
+        print("Cannot leave field empty.")
     
-    elif tickets == 3:
-        discount = price - (price * 0.05)
-        print("Your tickets cost AU$%.2f a pop, and AU$%.2f total." %(discount, discount*3))    
+    elif name == "Edu":
+        print("Nice name!")
 
-    elif tickets == 4:
-        discount = price - (price * 0.08)
-        print("Your tickets cost AU$%.2f a pop, and AU$%.2f total." %(discount, discount*4))
-
-    elif tickets > 4:
-        print("Tickets are sold out.")
-
-else:
-    print("Get lost, asshat!")
-
-print()
