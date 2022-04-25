22c791b94fae7e5aad995de8125fe7ab4f4f11d9
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-25 07:28:07 -0300

    Lambda more example and extra practice

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -1,3 +1,9 @@
+
+#? LAMBDA FUNCTIONS are written in one line using the lambda keyword
+#?  → they accept any number or arguments, but only have one expression
+
+#* FORMULA → lambda parameter: expression
+
 double = lambda x: x * 2
 # nearly the same as:
 # def double(x):
@@ -6,7 +12,15 @@ double = lambda x: x * 2
 print(double(5))
 print()
 
-# Filtering out onlyy the even items from a list
+multiply = lambda x, y: x * y
+print(multiply(10,11))
+print()
+
+full_name = lambda first_name, last_name: first_name + " " + last_name
+print(full_name("edu", "Vieira").capitalize())
+print()
+
+# Filtering out only the even items from a list
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
@@ -15,6 +29,7 @@ new_list = list(filter(lambda x: (x % 2 == 0), my_list))
 print("Original list:", my_list)
 print("Even numbers:", new_list)
 print()
+
 # Doubling each item in a list using map()
 
 new_list2 = list(map(lambda x: x * 2, my_list))
