cb59135ddfffa0d3c418bb363e9bbac51198066a
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-03 18:09:45 -0300

    Removing punctuation from string

---
+++ RemovePunctuation.py
@@ -0,0 +1,12 @@
+
+punct = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+message = "Hello, World! Welcome - and have fun..."
+
+no_punct = ""
+
+for char in message:
+    if char not in punct:
+        no_punct = no_punct + char
+        
+print(no_punct)
