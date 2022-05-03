820945828f2d95f74b420dc453b6f20008109f12
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-03 06:35:13 -0300

    The map() function

---
+++ MapFunction.py
@@ -0,0 +1,15 @@
+
+#? map() applies a function to each item in iterable (list, tuple, etc)
+# ma(function, iterable)
+
+store = [("shirt", 20.00),
+         ("pants", 27.00),
+         ("jacket", 50.00),
+         ("socks", 7.00)]
+
+USD_to_euros = lambda data: (data[0], data[1] * 0.92) #Converting to price of products to Euro
+                            # funxtion, iterable
+store_euro_price = list(map(USD_to_euros, store))
+
+for i in store_euro_price:
+    print("The {} cost(s) {:.2f} in Euros.".format(i[0], i[1]))
