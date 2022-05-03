debad0953916e01167a61b19b305c61aa142f09e
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-03 06:56:44 -0300

    The reduce() function

---
+++ Reduce.py
@@ -0,0 +1,17 @@
+
+#? reduce() applies a function to an iterable and reduce it to a single cumulative value.
+#      perfoms function on the first 2 elements and repeats process until 1 value remains.
+# reduce(function, iterable)
+
+import functools
+
+letters = ["E","D","U","A","R","D","O"]
+word = functools.reduce(lambda x, y: x + y, letters)
+
+print(word)
+print()
+
+factorial = [6,5,4,3,2,1]
+result = functools.reduce(lambda x, y: x * y, factorial)
+
+print(result)
