96b1299241f1a8e3b14b5c4a28f96d79a61b1d53
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-04 17:49:09 -0300

    minor adjustments

--- Literal_Collections.py
+++ Literal_Collections.py
@@ -1,7 +1,7 @@
 fruits = ["apple", "mango", "orange", "Pitaya"] #list
 numbers = (1, 2, 3, 4) #tuple
 alphabets = {'a':'apple', 'b':'ball', 'c':'cats'} #dictionary
-vowels = {'a', 'e', 'i', 'o', 'u'}
+vowels = {'a', 'e', 'i', 'o', 'u'} #set
 
 print(fruits)
 print(numbers)
