8fd1cdcdb038b9dc5cce8e91c3ec0f5f4f0c632b
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-08 10:43:41 -0300

    Changes

--- zw.py
+++ zw.py
@@ -1,29 +1,58 @@
-from time import sleep
 
-counter = 0
-max = 5
+#? Lists
 
-while counter < max:
-    print(counter)
-    sleep(0.5)
-    counter += 1
+food = ["pizza", "sushi", "burgers", "tacos", "spaghetti"]
 
+print(food) #prints entire list
+print(food[0]) #prints out the 1st element
+print()
 
-print('--  type QUIT to exit --')
+food[0] = "hotdog" #change index 0 value
+print(food[0])
+print()
 
-while True:
-    quit = input('\'quit\' or get stuck:')
-    if quit == 'quit':
-        break
+for x in food:
+    print(x)
 
-name = ""
+print()
 
-while name == "":
-    name = input("What's your name? ")
+# Functions
+
+food.append("pudding") # adds an element
+food.remove("tacos") # adios to tacos
+food.pop # removes last element
+food.insert(0, "shrimp") # adds element to a specific index
+food.sort() # in alphabetical order
+
+for x in food:
+    print(x)
+
+print()
+food.clear() # removes all of the elements
+print(food)
+
+#? A tuple is an oredered and unchangeable collection so we can group related data
+
+# instead of [] as in lists we use ()
+
+student = ("Edu", 21, "Male")
+
+print(student)
+# counts how many time a value appears
+print("Edu appears " + str(student.count("Edu")) + " times") 
+# shows the index of desired item
+print("The age is located at index " + (str(student.index(21))))
+print()
+
+# with a for loop
+for i in student:
+    print(i)
+
+print()
+
+# using if
+if 21 in student:
     
-    if name == "":
-        print("Cannot leave field empty.")
-    
-    elif name == "Edu":
-        print("Nice name! Good-bye")
+    print("Student is allowed to drink")change the 
+#? elements of a tuple once it is assigned whereas we can change the elements of a list.
 
