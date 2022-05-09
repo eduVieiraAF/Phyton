fdf6a808e7a9e9193c5a0cd2665b5c7261e8f74e
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-09 09:32:49 -0300

    chnages

--- zw.py
+++ zw.py
@@ -1,4 +1,6 @@
 
+#? Collections are often referred to as iterables
+
 #? Lists
 
 food = ["pizza", "sushi", "burgers", "tacos", "spaghetti"]
@@ -18,6 +20,7 @@ print()
 
 # Functions
 
+
 food.append("pudding") # adds an element
 food.remove("tacos") # adios to tacos
 food.pop # removes last element
@@ -31,7 +34,7 @@ print()
 food.clear() # removes all of the elements
 print(food)
 
-#? A tuple is an  unchangeable collection so we can group related data
+#? A tuple is an unchangeable collection so we can group related data
 
 # instead of [] as in lists we use ()
 
@@ -42,7 +45,9 @@ print(student)
 print("Edu appears " + str(student.count("Edu")) + " times") 
 # shows the index of desired item
 print("The age is located at index " + (str(student.index(21))))
+print(len(student))
 print()
+
 
 # with a for loop
 for i in student:
@@ -57,10 +62,9 @@ if 21 in student:
 
 print()
 
-#? a set is a collection that does not allow duplicate values
+#? a set is a collection that does not allow duplicate values and element reassignment
 
-
-utensils = {"fork", "spoon", "knife", "mug"}
+utensils = {"fork", "spoon", "knife", "chopsticks"}
 dishes = {"bowl", "plate", "mug"}
 dinner_table = utensils.union(dishes) # joins both sets
 
@@ -72,9 +76,38 @@ utensils.remove("knife")
 print(utensils)
 print()
 
-utensils.update(dishes) # adds all the items to a new set
+utensils.update(dishes) # adds all the items to one of the sets
 print(utensils)
 print()
 
 print(utensils.difference(dishes)) # display what one has and other doesn't
 print(utensils.intersection(dishes)) # displays what they have in common
+dinner_table.clear() #removes all items
+
+# a dictionary is a changeable, unordered collection of unique keys/value pair
+# which is fast because they use hashing. Similar to set
+
+capitals = {'USA': 'Washinton DC', 
+            "Brazil": "Brasilia", 
+            "India": "New Dehli", 
+            "Canada": "Ottawa", 
+            "Australia": "Canberra",
+            "Russia": "Moscow"}
+
+print(capitals["Australia"])
+print(capitals.get('Japan')) # safer way to access dictionaries
+print(capitals.keys()) # prints keys, not values
+print(capitals.values()) # prints all values
+print(capitals.items()) # prints keys and values
+
+capitals.update({"Germany": "Berlin"}) # can add new item
+capitals.update({'India': 'Dehli'}) # or alter existing value
+capitals.pop("USA") # removes item
+
+print()
+
+for key, value in capitals.items():
+    print(key, value)
+
+print()
+capitals.clear() # deletes every item
