769852c8d1479342c0d7f5c52273e0e06bec92fe
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-23 16:54:38 -0300

    Creating a button

---
+++ Tkinter/buttons.py
@@ -0,0 +1,33 @@
+
+from tkinter import *
+
+count = 0
+
+def click():
+    global count
+    count += 1
+    print("You've clicked {} times.".format(count))
+
+window = Tk()
+window.geometry("460x460")
+window.title("Phoenix code")
+window.iconbitmap('./Tkinter/Phoenix.ico')
+
+photo = PhotoImage(file='./Tkinter/spaceship.png')
+
+button = Button(
+    window, 
+    text="Click me!",
+    command=click,
+    font=("Comic Sans", 40),
+    fg="#e7eef5",
+    bg="Black",
+    activeforeground="#562f7e",
+    activebackground="Black",
+    image=photo,
+    compound="bottom"
+    )
+
+button.pack()
+
+window.mainloop()
