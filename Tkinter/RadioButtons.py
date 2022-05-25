05cc56d21617a8f348c5bf5c2d10ed36a3ac9498
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-25 09:19:00 -0300

    RadioButton

---
+++ Tkinter/RadioButtons.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+food = ["Pizza", "Hamburger", "Hotdog"]
+
+def order():
+    if x.get() == 0:
+        print("Pizza! Good ordering.")
+    
+    elif x.get() == 1:
+        print("Hmmm... Burger!")
+        
+    else:
+        print("A hotdog might hit the spot.")
+
+window = Tk()
+
+x = IntVar()
+
+window.title("Python code")
+window.iconbitmap('./Tkinter/python.ico')
+
+pizza_image = PhotoImage(file='./Tkinter/pizza.png')
+burger_image = PhotoImage(file='./Tkinter/burger.png')
+hotdog_image = PhotoImage(file='./Tkinter/hotdog.png')
+
+food_images = [pizza_image, burger_image, hotdog_image]
+
+for index in range(len(food)):
+    radiobutton = Radiobutton(
+        window, 
+        text=food[index],
+        variable=x,
+        value = index,
+        padx=25,
+        pady=10,
+        font=("Impact", 30),
+        image=food_images[index],
+        compound="left",
+        indicatoron=0,
+        width=225,
+        command=order
+        )
+    radiobutton.pack(anchor=W)
+
+window.mainloop()
