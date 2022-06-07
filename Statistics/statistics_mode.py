49ee77c393f4b4fa7320fa6bc2a3f891998f1db1
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-07 11:53:22 -0300

    Mode calculates central tendency of the given numeric or nominal data set

---
+++ Statistics/statistics_mode.py
@@ -0,0 +1,8 @@
+
+#? The statistics.mode() method calculates the mode (central tendency) of the given numeric or nominal data set.
+
+import statistics
+
+print(statistics.mode([1, 3, 3, 3, 5, 7, 7, 9, 11]))
+print(statistics.mode([1, 1, 3, -5, 7, -9, 11]))
+print(statistics.mode(['red', 'green', 'blue', 'red']))
