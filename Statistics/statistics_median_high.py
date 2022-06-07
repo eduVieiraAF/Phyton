10a214c83258a6f27d6b2ead6373a0d02e73346b
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-06 23:25:47 -0300

    low median

--- Statistics/statistics_median_high.py
+++ Statistics/statistics_median_high.py
@@ -12,3 +12,5 @@ import statistics
 print(statistics.median_high([1, 3, 5, 7, 9, 11, 13]))
 print(statistics.median_high([1, 3, 5, 7, 9, 11]))
 print(statistics.median_high([-11, 5.5, -3.4, 7.1, -9, 22]))
+
+#* Also → statistics.median_low() method calculates the low median of the given data set.
