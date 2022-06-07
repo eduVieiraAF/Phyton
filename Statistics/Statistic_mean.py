143d327b539b43e20e64dbdd611fde1b19150beb
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-06 23:03:53 -0300

    Statistics mean

---
+++ Statistics/Statistic_mean.py
@@ -0,0 +1,12 @@
+
+#? Statistics mean
+
+#*The statistics.mean() method calculates the mean (average) of the given data set.
+
+# Mean = add up all the given values, then divide by how many values there are.
+
+import statistics
+
+print(statistics.mean([1, 3, 5, 7, 9, 11, 13]))
+print(statistics.mean([1, 3, 5, 7, 9, 11]))
+print("{:.2f}".format(statistics.mean([-11, 5.5, -3.4, 7.1, -9, 22])))
