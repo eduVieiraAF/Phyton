80223d1eb1d0ecf37090ce774dd0a1c8d8e83f14
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-27 18:36:03 -0300

    Changes

--- Tkinter/Window.py
+++ Tkinter/Window.py
@@ -3,8 +3,8 @@ import tkinter as tk
 window1 = tk.Tk()
 window1.title('Window 1 → centered')
 
-window_width = 300
-window_height = 200
+window_width = 460
+window_height = 460
 
 # get the screen dimension
 screen_width = window1.winfo_screenwidth()
@@ -31,7 +31,7 @@ window2.mainloop()
 window3 = tk.Tk()
 
 window3.title('Window 3 → transparent')
-window3.geometry('600x400+180+120')
+window3.geometry('600x400')
 window3.attributes('-alpha', 0.7)
 
 window3.mainloop()
@@ -39,7 +39,7 @@ window3.mainloop()
 window4 = tk.Tk()
 
 window4.title('Window 4 - with personalized icon')
-window4.geometry('1000x400+150+150')
+window4.geometry('1000x400')
 window4.resizable(False, True)
 window4.iconbitmap('./Tkinter/Phoenix.ico')
 
--- zw.py
+++ zw.py
@@ -27,6 +27,54 @@ shutil.copyfile('test.txt', 'text_copy.txt') # source, destination/create file's
 
 #label is an area widget that holds text and/or image
 
+import tkinter as tk
+
+window1 = tk.Tk()
+window1.title('Window 1 → centered')
+
+window_width = 460
+window_height = 460
+
+# get the screen dimension
+screen_width = window1.winfo_screenwidth()
+screen_height = window1.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+window1.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+window1.mainloop()
+
+window2 = tk.Tk()
+
+window2.title('Window 2 → sized manually and not resizable')
+window2.geometry('1000x400+50+50') # windown.geometry('width x height ± x ± y)
+window2.resizable(False, False)
+
+window2.mainloop()
+
+# Transparency
+
+window3 = tk.Tk()
+
+window3.title('Window 3 → transparent')
+window3.geometry('600x400')
+window3.attributes('-alpha', 0.7)
+
+window3.mainloop()
+
+window4 = tk.Tk()
+
+window4.title('Window 4 - with personalized icon')
+window4.geometry('1000x400')
+window4.resizable(False, True)
+window4.iconbitmap('./Tkinter/Phoenix.ico')
+
+window4.mainloop()
+
+
 from tkinter import *
 
 window = Tk()
