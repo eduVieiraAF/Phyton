e0bb3f5dcafdf04c7a50905b6d99cb8ccf436768
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-22 09:42:12 -0300

    Separation of file: → Breaking the code into more files

--- Tkinter/Calculator/Calculator.py
+++ Tkinter/Calculator/Calculator.py
@@ -1,11 +1,6 @@
 from tkinter import *
 from values import *
-
-calc_window = Tk()
-calc_window.title("Totalizer")
-#calc_window.geometry("330x450")
-calc_window.config(padx=8, pady=8, relief="raised", bd=5, background="#132020")
-calc_window.iconbitmap("C:\\Users\\edu_v\\Python\\Tkinter\\Calculator\\Images\\calc.ico")
+from Main import calc_window
 
 frm_outline = Frame(
     calc_window,
@@ -18,14 +13,15 @@ frm_outline = Frame(
 
 lbl_display = Label(
     calc_window,
-    text="1234567890+-/*=CE",
+    text="0",
     bg=primary_dark_variant,
     fg=secondary_light_variant,
     font=(lbl_font, 23,"bold"),
     relief="sunken",
     bd=7,
+    width=15,
     padx=10,
-    pady=10
+    pady=10,
     )
 
 button_ce = Button(
@@ -140,17 +136,60 @@ button_plus = Button(
     pady=12
 )
 
-lbl_display.pack(side=TOP)
-frm_outline.pack(side=BOTTOM)
+button4 = Button(
+    frm_outline,
+    bg=primary_dark_variant,
+    activebackground=primary_light_variant,
+    activeforeground=primary_dark_variant,
+    fg="white",
+    text="4",
+    font=(btn_font, 16, "bold"),
+    height=btn_height,
+    width=btn_width,
+    padx=12,
+    pady=12
+)
 
-button_ce.grid(row=0,column=0)
-button_div.grid(row=0,column=1)
-button_times.grid(row=0,column=2)
-button_minus.grid(row=0,column=3)
+button5 = Button(
+    frm_outline,
+    bg=primary_dark_variant,
+    activebackground=primary_light_variant,
+    activeforeground=primary_dark_variant,
+    fg="white",
+    text="5",
+    font=(btn_font, 16, "bold"),
+    height=btn_height,
+    width=btn_width,
+    padx=12,
+    pady=12
+)
 
-button7.grid(row=1,column=0)
-button8.grid(row=1,column=1)
-button9.grid(row=1,column=2)
-button_plus.grid(row=1,column=3)
+button6 = Button(
+    frm_outline,
+    bg=primary_dark_variant,
+    activebackground=primary_light_variant,
+    activeforeground=primary_dark_variant,
+    fg="white",
+    text="6",
+    font=(btn_font, 16, "bold"),
+    height=btn_height,
+    width=btn_width,
+    padx=12,
+    pady=12
+)
 
-calc_window.mainloop()
+button_dot = Button(
+    frm_outline,
+    bg=secondary_light_variant,
+    activebackground=secondary_dark_variant,
+    activeforeground=primary_light_variant,
+    fg=primary_dark_variant,
+    text=".",
+    font=(btn_font, 16, "bold"),
+    height=btn_height,
+    width=btn_width,
+    padx=12,
+    pady=12
+)
+
+
