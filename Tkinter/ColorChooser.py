7d85c66b9914b43a79e3c8babe47bdd94f398e8e
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-23 12:01:06 -0300

    ColorChooser

--- Tkinter/ColorChooser.py
+++ Tkinter/ColorChooser.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import colorchooser
+from tkinter import messagebox
 
 window = Tk()
 window.title("Python coding by Edu")
-window.geometry("450x450")
+window.geometry("450x450+300+200")
 window.iconbitmap('./Tkinter/python.ico')
 
 def choose_color():
@@ -11,10 +12,11 @@ def choose_color():
     color_hex = color[1]
     button.config(fg=color_hex)
     window.config(bg=color_hex)
+    messagebox.showinfo(title= "Hexadecimal value", message= "Here's the color you've picked out → "+ color_hex)
 
 button = Button(
     text="Choose\n color",
-    font=("Arial", 20),
+    font=("Arial", 20,"bold"),
     command=choose_color
 )
 
