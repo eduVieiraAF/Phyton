7cb434a733bedc1ad7f12df2fd96c7334a76bb66
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-27 11:37:50 -0300

    Keyboard events

---
+++ Tkinter/KeyboardEvents.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import messagebox
+
+def warn(event):
+    messagebox.showwarning(title="WARNING", message="You've hit the {} key!".format(event.keysym))
+
+window = Tk()
+window.title("Python coding by Edu")
+window.geometry("460x460")
+window.iconbitmap('./Tkinter/python.ico')
+
+window.bind("<Key>", warn)
+
+window.mainloop()
