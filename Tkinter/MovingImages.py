6410b26372e1b885edf90a7fc4979a9bf7666c64
Edu Vieira <edu_vieira@hotmail.com>
2022-08-15 16:36:51 -0300

    Moving images with keyboard events

---
+++ Tkinter/MovingImages.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+
+def move_up(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y() - 10)
+
+
+def move_down(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y() + 10)
+
+
+def move_left(event):
+    label.place(x=label.winfo_x() - 10, y=label.winfo_y())
+
+
+def move_right(event):
+    label.place(x=label.winfo_x() + 10, y=label.winfo_y())
+
+
+window = Tk()
+window.title("Moving objects")
+window.geometry("500x500")
+window.config(bg="black")
+
+window.bind("<Up>", move_up)
+window.bind("<Down>", move_down)
+window.bind("<Left>", move_left)
+window.bind("<Right>", move_right)
+
+spaceship = PhotoImage(file="spaceship.png")
+
+label = Label(
+    window,
+    image=spaceship,
+    bg="black"
+)
+
+label.place(x=0, y=0)
+
+window.mainloop()
