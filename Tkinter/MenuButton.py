9ba99b3c263dad2ff79ef79bc059951d91b267e1
Edu Vieira <edu_vieira@hotmail.com>
2022-08-16 12:57:56 -0300

    Simple menu button sample

---
+++ Tkinter/MenuButton.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+def esc(event):
+    if messagebox.askyesno(title="Exit?", message="Are you sure you want to quit?"):
+        window.destroy()
+
+
+window = Tk()
+window.title("Menu button")
+window.geometry("460x460")
+window.config(pady=20)
+window.bind("<Escape>", esc)
+
+mb = Menubutton(
+    window,
+    text="Condiments",
+    relief="groove"
+)
+mb.grid()
+mb.menu = Menu(mb, tearoff=0)
+mb["menu"] = mb.menu
+
+mayoVar = IntVar()
+ketchVar = IntVar()
+
+mb.menu.add_checkbutton(
+    label="Mayo",
+    variable=mayoVar
+)
+mb.menu.add_checkbutton(
+    label="Ketchup",
+    variable=ketchVar
+)
+
+mb.pack()
+
+window.mainloop()
