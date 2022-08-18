592e3c3fc429775ebddfc5d01dc09391f6b83dff
Edu Vieira <edu_vieira@hotmail.com>
2022-08-17 21:10:43 -0300

    touching on time && clock

--- Clock.py
+++ Clock.py
@@ -8,7 +8,7 @@ root.config(padx=15, pady=15, bg="black")
 
 
 def time():
-    string = strftime('%a | %B %d, %Y\n%H:%M:%S %p')
+    string = strftime('%A\n%B %d, %Y\n%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
 
@@ -19,7 +19,7 @@ label = Label(
     background="black",
     foreground="#ff5c40"
 )
-label.pack(anchor='center')
+label.pack()
 
 time()
 
