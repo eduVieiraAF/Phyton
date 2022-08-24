86cd8792aae78035f05428b7ce208c2e46c23f26
eduVieiraAF <edu_vieira@hotmail.com>
2022-08-24 17:44:54 -0300

    Playing around a bit with animations

--- Tkinter/Animation_Multiple.py
+++ Tkinter/Animation_Multiple.py
@@ -4,6 +4,7 @@ from AnimationBall import *
 
 window = Tk()
 window.title("Multiple objects in animation")
+window.resizable(False, False)
 
 WIDTH = 500
 HEIGHT = 500
@@ -18,14 +19,16 @@ canvas = Canvas(
 
 canvas.pack()
 
-volley_ball = Ball(canvas, 0, 0, 100, X_VELOCITY-1, Y_VELOCITY+1, "#eeedea")
-basket_ball = Ball(canvas, 0, 0, 110, X_VELOCITY+1, Y_VELOCITY+6, "#d44500")
+big_ball = Ball(canvas, 0, 0, 200, X_VELOCITY-1, Y_VELOCITY-1, "#ffb9cd")
+volley_ball = Ball(canvas, 0, 0, 80, X_VELOCITY-1, Y_VELOCITY+1, "#eeedea")
+basket_ball = Ball(canvas, 0, 0, 110, X_VELOCITY+1, Y_VELOCITY+5, "#d44500")
 tennis_ball = Ball(canvas, 0, 5, 40, X_VELOCITY, Y_VELOCITY+3, "#aea600")
 soccer_ball = Ball(canvas, 0, 0, 60, X_VELOCITY + 1, Y_VELOCITY + 2, "#515b62")
-marble1 = Ball(canvas, 0, 0, 5, X_VELOCITY+4, Y_VELOCITY+3, "#515b62")
-marble2 = Ball(canvas, 0, 0, 5, X_VELOCITY+3, Y_VELOCITY+3, "#174071")
+marble1 = Ball(canvas, 0, 0, 5, X_VELOCITY+7, Y_VELOCITY+5, "#515b62")
+marble2 = Ball(canvas, 0, 0, 8, X_VELOCITY+3, Y_VELOCITY, "#49b675")
 
-while True:
+while True:    
+    big_ball.move()
     volley_ball.move()
     basket_ball.move()
     tennis_ball.move()
@@ -35,4 +38,4 @@ while True:
     window.update()
     time.sleep(0.01)
 
-window.mainloop()
+# window.mainloop()
