ed0d91c644a0835545c2da6173adf1ea4e00909d
eduVieiraAF <edu_vieira@hotmail.com>
2022-10-05 09:19:09 -0300

    the walrus operator

--- WalrusOperator.py
+++ WalrusOperator.py
@@ -1,10 +1,24 @@
+
 # ? Walrus operator := is an assignment expression
 # * assigns values to a variable as part of a larger expression
 
-# happy = True
-# print(happy)
+happy = True
+if happy:
+    print(happy)
 
 print(happy := True)
+
+a = [1, 2, 3, 4]
+n = len(a)
+if n > 3:
+    print(f"List is too long ({n} elements, expected <= 3)")
+
+
+#a = [1, 2, 3, 4]
+#if (n := len(a)) > 3:
+#	print(f"List is too long ({n} elements, expected <= 3)")
+
+
 
 foods = list()
 
@@ -14,5 +28,27 @@ foods = list()
 #        break
 #    foods.append(food)    without the walrus operator
 
-while food := input("What food do you like? ") != "quit":
+while food := input("What food do you like? ").lower() != "quit":
     foods.append(food)
+
+
+sample_data = [
+	{"userId": 1, "name": "rahul", "completed": False},
+	{"userId": 2, "name": "rohit", "completed": False},
+	{"userId": 3, "name": "ram", "completed": False},
+	{"userId": 4, "name": "ravan", "completed": True}
+]
+
+print("Without Walrus operator:")
+for entry in sample_data:
+	name = entry.get("name")
+	if name:
+		print(f'Found name: "{name}"')
+
+
+print("With Walrus Operator:")
+for entry in sample_data:
+	if name := entry.get("name"):
+		print(f'Found name: "{name}"')
+
+
