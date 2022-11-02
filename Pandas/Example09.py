6a841d9f7638c53ea3a2258151ad8eec4a21683b
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-02 10:08:21 -0300

    fixing plot

--- Pandas/Example09.py
+++ Pandas/Example09.py
@@ -1,17 +1,10 @@
-#Three lines to make our compiler able to draw:
-import sys
-import matplotlib
-matplotlib.use('Agg')
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Pandas\data.csv')
+df = pd.read_csv('data.csv')
 
-df.plot(kind = 'scatter', x = 'Duration', y = 'Calories')
+df.plot(kind='scatter', x='Duration', y='Calories')
 
 plt.show()
 
-#Two  lines to make our compiler able to draw:
-plt.savefig(sys.stdout.buffer)
-sys.stdout.flush()
