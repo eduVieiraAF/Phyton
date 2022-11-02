b703dec6f2d9c00924f909b0421e682847bc8a51
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-02 10:38:58 -0300

    Markers

---
+++ Matplotlib/Example06.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y_point = np.array([3, -8, 2, -10, 25, -69])
+
+plt.plot(y_point, marker='_')
+plt.show()
+
+# * MARKERS
+"""
+'o'	Circle
+'*'	Star
+','	Pixel
+'x'	X
+'X'	X (filled)
+'+'	Plus
+'P'	Plus (filled)
+'s'	Square
+'D'	Diamond
+'d'	Diamond (thin)
+'p'	Pentagon
+'H'	Hexagon
+'h'	Hexagon
+'v'	Triangle Down
+'^'	Triangle Up
+'<'	Triangle Left
+'>'	Triangle Right
+'1'	Tri Down
+'2'	Tri Up
+'3'	Tri Left
+'4'	Tri Right
+'_'	H line
+"""
