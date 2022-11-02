38959167cd2558d846e1b87153c6f659d1fc8bd6
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-02 10:25:03 -0300

    Plotting with default x-point

---
+++ Matplotlib/Example05.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y_points = np.array([3, 8, 1, 10, 5, 7])
+# The x_points in the example above is [0, 1, 2, 3, 4, 5].
+
+plt.plot(y_points)
+plt.show()
