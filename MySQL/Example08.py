c4820cb0918afabd28c5e3ab846d2ab3876a63c4
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-15 19:25:30 -0300

    fixing typo

--- MySQL/Example08.py
+++ MySQL/Example08.py
@@ -5,9 +5,9 @@ my_cursor = myDB.cursor()
 # command = "SELECT * FROM users WHERE id > 2"
 # command = "SELECT * FROM users WHERE name LIKE '%u%'"
 
-#? Query with placeholder and seperate variable with values
+# ? Query with placeholder and separate variable with values
 command = "SELECT * FROM users WHERE name LIKE %s"
-value = ("%r%",)
+value = ("%o%",)
 
 my_cursor.execute(command, value)
 my_query = my_cursor.fetchall()
