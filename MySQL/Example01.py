1602f82d2c8ca8d61c94929af0a56e9d17a4b236
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-14 22:35:12 -0300

    MySQL connecting with MySQL

---
+++ MySQL/Example01.py
@@ -0,0 +1,5 @@
+
+#* Checking connection with MySQL
+from Info import myDB
+
+print(myDB)
---
+++ MySQL/Info.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+myDB = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="somepassword"
+)
