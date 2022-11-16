5603d71140ebc29b6850cc3da85ba95401d7c379
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-15 21:27:15 -0300

    Adding a dictionary

--- MongoDB/Client.py
+++ MongoDB/Client.py
@@ -2,3 +2,4 @@ import pymongo
 
 my_client = pymongo.MongoClient('mongodb://localhost:27017/')
 my_db = my_client["PyDatabase"]
+my_collection = my_db["users"]
---
+++ MongoDB/Example05.py
@@ -0,0 +1,9 @@
+from Client import *
+
+# * Adding a dictionary
+
+my_dic = {"name": "Edu", "email": "edu@edu.com.br"}
+
+x = my_collection.insert_one(my_dic)
+
+print(x)
