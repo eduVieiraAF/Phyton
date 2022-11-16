c5fe5dfdee6683a1c684ed4d066a4d26966a6fd4
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-16 06:41:16 -0300

    Finding documents - more advanced

--- MongoDB/Example07.py
+++ MongoDB/Example07.py
@@ -16,6 +16,32 @@ for doc in my_collection.find({}, {"_id": 0, "name": 1, "email": 1}):
 for doc in my_collection.find({}, {"name": 0}):
     print(doc)
 
+# checking for email
 for doc in my_collection.find({}, {"_id": 0, "name": 0}):
     if doc == {"email": "molz@gmail.com"}:
         print("Molly is in document")
+
+
+my_query = {"email":"edu@edu.com"}
+my_doc = my_collection.find(my_query)
+
+for i in my_doc:
+    print(i)
+
+# Greater than modifier $gt
+
+print()
+my_query = {"email": {"$gt": "l"}}
+my_doc = my_collection.find(my_query)
+
+for x in my_doc:
+    print(x)
+
+print()
+
+# query with RegEx
+my_query = {"email": {"$regex": "^e"}}
+my_doc = my_collection.find(my_query)
+
+for x in my_doc:
+    print(x)
