344270a831d2d2b361c43a21d6b2b31cb47af68c
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-20 20:49:30 -0300

    Changing things around

--- MapFunction.py
+++ MapFunction.py
@@ -4,11 +4,11 @@
 store = [("shirt", 15.00),
          ("pants", 27.00),
          ("jacket", 50.00),
-         ("socks", 5.00)]
+         ("socks", 8.00)]
 
-USD_to_Real = lambda data: (data[0], (data[1] * 3.6) * 1.25)  # Converting to price of products to Real
+AUD_to_Real = lambda data: ((data[0], ((data[1] * 3.6) * 1.15)*1.40))  # Converting to price of products to Real + taxes + IOF
 # function, iterable
-store_real_price = list(map(USD_to_Real, store))
+store_real_price = list(map(AUD_to_Real, store))
 
 for i in store_real_price:
     print("The {} cost(s) {:.2f} in Reals.".format(i[0], i[1]))
