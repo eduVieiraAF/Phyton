607f6b0c8a512cbf2eb78f9ab522f6fd41c4b6fc
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-20 20:36:51 -0300

    Changing around

--- Tkinter/ProgressBar.py
+++ Tkinter/ProgressBar.py
@@ -1,52 +1,32 @@
-import time
 from tkinter import *
 from tkinter.ttk import *
+import time
 
 
 def start():
-    tasks = 500
-    x = 0
-    speed = 6
-    while x < tasks:
+    GB = 100
+    download = 0
+    speed = 3
+    while (download < GB):
         time.sleep(0.05)
-        pb['value'] += (speed / tasks) * 100
-        x += speed
-        task.set(str(x) + "/" + str(tasks) + " tasks completed")
-        percent.set(str(int((x / tasks) * 100)) + "%")
+        bar['value'] += (speed/GB)*100
+        download += speed
+        percent.set(str(int((download/GB)*100))+"%")
+        text.set(str(download)+"/"+str(GB)+" GB completed")
         window.update_idletasks()
 
 
 window = Tk()
-window.title("Python coding by Edu")
-window.config(padx=25, pady=25)
 
 percent = StringVar()
-task = StringVar()
+text = StringVar()
 
-pb = Progressbar(
-    window,
-    orient=HORIZONTAL,
-    length=300,
-)
+bar = Progressbar(window, orient=HORIZONTAL, length=300)
+bar.pack(pady=10, padx=10)
 
-pb.pack(pady=10)
+percentLabel = Label(window, textvariable=percent).pack()
+taskLabel = Label(window, textvariable=text).pack()
 
-Label(
-    window,
-    textvariable=percent
-
-).pack()
-
-Label(
-    window,
-    textvariable=task
-
-).pack()
-
-Button(
-    window,
-    text="Download",
-    command=start
-).pack(pady=5)
+button = Button(window, text="download", command=start).pack()
 
 window.mainloop()
