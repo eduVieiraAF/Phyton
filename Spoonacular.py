b029d965bf9bb0332dad9f07244463df63e1ecf9
Eduardo Vieira <edu_vieira@hotmail.com>
2022-12-17 14:13:33 -0300

    playing around

--- Spoonacular.py
+++ Spoonacular.py
@@ -9,28 +9,20 @@ endpoint = "https://api.spoonacular.com/recipes/findByIngredients"
 # Set the parameters for the API call
 params = {
     "apiKey": api_key,
-    "ingredients": "prawn, honey, chilli",
-    #"number": 5,
-    #"ranking": 2
+    "ingredients": "sausage",
 }
 
-# Make the API call and get the response
 response = requests.get(endpoint, params=params)
 
-# Print the response status code to make sure the call was successful
 print(response.status_code)
 
-# Print the response body to see the results of the call
-print(response.text)
+# print(response.text)
 
 if response.status_code == 200:
-    # Convert the response to a Python dictionary
     response_dict = response.json()
 
-    # Print the dictionary to see the structure of the response
-    print(response_dict)
+    # print(response_dict)
 
-    # Access individual elements of the response
     print(response_dict[0]["id"])
     print(response_dict[0]["image"])
     print(response_dict[0]["title"])
