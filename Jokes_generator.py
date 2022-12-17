9b5885a9cdbc02e78d6f44e761f6f46cbb777fb2
Eduardo Vieira <edu_vieira@hotmail.com>
2022-12-17 11:17:55 -0300

    Joke generator

---
+++ Jokes_generator.py
@@ -0,0 +1,39 @@
+import json
+
+import requests
+
+
+def tell_joke():
+    jokes = []
+
+    for i in range(0, 5):
+        joke = get_joke()
+        jokes.append(joke)
+
+    for joke in jokes:
+        print(joke.setup)
+        print(joke.punchline)
+        print("")
+
+
+class Joke:
+    def __init__(self, id, type, setup, punchline):
+        self.id = id
+        self.type = type
+        self.setup = setup
+        self.punchline = punchline
+
+
+def get_joke():
+    endpoint = "https://official-joke-api.appspot.com/random_joke"
+
+    response = requests.get(endpoint)
+
+    joke_dict = json.loads(response.text)
+
+    joke = Joke(joke_dict['id'], joke_dict['type'], joke_dict['setup'], joke_dict['punchline'])
+
+    return joke
+
+
+tell_joke()
