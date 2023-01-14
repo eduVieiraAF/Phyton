f66c771bdd2d75648d51b3ec3c73065f023ee236
Eduardo Vieira <edu_vieira@hotmail.com>
2023-01-14 15:04:22 -0300

    pytube

---
+++ YouTube.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+# URL of the video
+url = "https://www.youtube.com/watch?v=jnfTN5c41Ug"
+
+# Create a YouTube object
+yt = YouTube(url)
+
+# Play the video
+yt.streams.first().download()
