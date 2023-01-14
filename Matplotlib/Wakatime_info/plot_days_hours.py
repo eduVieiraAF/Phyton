6fc2db7b8478420e416bd12fb756288b4591f2c6
Eduardo Vieira <edu_vieira@hotmail.com>
2023-01-14 17:23:52 -0300

    pie

--- Matplotlib/Wakatime_info/plot_days_hours.py
+++ Matplotlib/Wakatime_info/plot_days_hours.py
@@ -5,11 +5,9 @@ with open('Matplotlib\Wakatime_info\days_hours.json') as file:
     data = json.load(file)
     
 hours = [item["grand_total"]["hours"] for item in data["data"]]
+
 dates = [item["range"]["date"] for item in data["data"]]
 
-plt.bar(dates, hours, color="#00748e" )
-plt.xlabel("Dates")
-plt.ylabel("Hours")
+plt.pie(hours, labels=dates, autopct='%1.1f%%')
 plt.title("Hours per day")
-
 plt.show()
