702cde6c2e7760ea7d6e6afd52ff145822b97e0a
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-28 09:10:30 -0300

    changes

--- Harris_benedict_equation.py
+++ Harris_benedict_equation.py
@@ -4,13 +4,13 @@ def calculate_bmr(weight, height, age, sex):
     elif sex.lower() == "female":
         bmr = 447.593 + (9.247 * weight) + (3.098 * height * 100) - (4.330 * age)
     else:
-        raise ValueError("Invalid sex provided. Please enter 'male' or 'female'.")
+        raise ValueError(sex.upper(), "Invalid sex provided. Please enter 'male' or 'female'.")
     return bmr
 
-# Example usage
-weight = 76.8  # in kilograms
-height = 1.66  # in meters
-age = 40  # in years
+
+weight = 76.8 
+height = 1.66 
+age = 41  
 sex = "male"
 
 bmr = calculate_bmr(weight, height, age, sex)
