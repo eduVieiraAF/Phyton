994a8f7eb2c1cbc038b4560d750d3dd7ef708592
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2024-10-21 07:41:58 -0300

    labels in PYQT5

---
+++ PYQT5/02_labels.py
@@ -0,0 +1,40 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Labels")
+        self.setGeometry(200, 200, 520, 600)
+        self.setWindowIcon(QIcon("PYQT5/python.png"))
+        
+        label = QLabel("Hello World", self)
+        label.setGeometry(20, 20, 400, 150)
+        label.setFont(QFont("Arial", 25))
+        label.setStyleSheet(
+            "color: cyan;" 
+            "background-color: black;"
+            "font-weight: bold;"
+            "border-style: solid;"
+            "border-width: 2px;"
+            "border-color: green;"
+            )
+        # label.setAlignment(Qt.AlignTop)
+        label.setAlignment(Qt.AlignCenter)
+        label.show()
+        label.show()
+        
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+
